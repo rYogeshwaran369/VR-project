8f6fcf713f51abdc9b3d8e7df2690d59be6ae219
rYogeshwaran369 <ryogeshwaran369@gmail.com>
2023-05-18 11:14:49 +0530

    bd

--- src/App.js
+++ src/App.js
@@ -8,17 +8,7 @@ import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    // <div className="App">
-    //   <HomePage/>
-    //   {/* <Content/> */}
-    //   {/* <Project1/> */}
-    // </div>
-    <Routes>
-      
-      <Route path='/' element={<HomePage/>}></Route>
-      <Route path='/content' element={<Content/>}></Route>
-      <Route path='/project1' element={<Project1/>}></Route>
-    </Routes>
+    <h1>AI-CLUB</h1>
   );
 }
 
